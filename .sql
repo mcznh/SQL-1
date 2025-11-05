5c4419e695135ae05c300504b5b7f8b1f30d6676
Maria Carolina Frota <94071764+mcznh@users.noreply.github.com>
2025-11-05 15:47:15 -0300

    Add SQL queries to retrieve book titles and authors

---
+++ .sql
@@ -0,0 +1,11 @@
+-- Return all titles from the books table
+SELECT title
+FROM books;
+
+-- Select title and author from the books table
+SELECT title, author
+FROM books;
+
+-- Select all fields from the books table
+SELECT *
+FROM books;
