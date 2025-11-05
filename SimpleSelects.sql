58571ac53ec01bf9ca7856c8ea28b0a38b1df5b6
Maria Carolina Frota <94071764+mcznh@users.noreply.github.com>
2025-11-05 16:49:14 -0300

    Create view for unique authors from books

--- SimpleSelects.sql
+++ SimpleSelects.sql
@@ -9,3 +9,11 @@ FROM books;
 -- Select all fields from the books table
 SELECT *
 FROM books;
+
+-- Your code to create the view:
+CREATE VIEW library_authors AS
+SELECT DISTINCT author AS unique_author
+FROM books;
+
+-- Select all columns from library_authors
+SELECT * FROM library_authors;
