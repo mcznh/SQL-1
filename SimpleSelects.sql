06d2536560b5477a655074a76aef67cc87c2578e
Maria Carolina Frota <94071764+mcznh@users.noreply.github.com>
2025-11-05 17:03:13 -0300

    Add query to select first 10 genres from books
    Added a query to select the first 10 genres from books.

--- SimpleSelects.sql
+++ SimpleSelects.sql
@@ -17,3 +17,8 @@ FROM books;
 
 -- Select all columns from library_authors
 SELECT * FROM library_authors;
+
+-- Select the first 10 genres from books using PostgreSQL
+SELECT genre 
+FROM books
+LIMIT 10;
